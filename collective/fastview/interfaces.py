9575c8c4c8f8568c6566401bd0118af17313ddf8
Mikko Ohtamaa <mikko@opensourcehacker.com>
2010-11-04 22:03:26 +0000

    Prepare collective.fastview 0.2.1.

--- collective/fastview/interfaces.py
+++ collective/fastview/interfaces.py
@@ -7,5 +7,9 @@ class ViewletNotFoundException(RuntimeError):
     """ Special exception for the cases we didn't found a viewlet the user asked through traversing """
 
 class ViewletProcessingFailsException(RuntimeError):
-    """ Special exception for the cases viewlet update() or render() raises exception """
+    """ Special exception for the cases viewlet update() or render() raises exception.
+    
+    If we let exceptions like TraversalError or NotFound through,
+    the orignal exception cause is lost.
+    """
         
